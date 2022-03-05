20a4ced5b0d209211c62f7d9c3aad1f913fd4b0c
Gamilkar <gamilkarr@gmail.com>
2022-03-05 16:13:03 +0300

    change routes
    add controller
    change templates

---
+++ main/controller.py
@@ -0,0 +1,10 @@
+def get_animal_list():
+    pass
+
+
+def get_animal():
+    pass
+
+
+def get_procedure_list():
+    pass
--- main/routes.py
+++ main/routes.py
@@ -1,23 +1,29 @@
-from main import app
+from main import app, controller
 from flask import render_template
 
 
 @app.route("/")
 def index():
-    return render_template("main/index.html")
+    animals = controller.get_animal_list()
+    context = {"animals": animals}
+    return render_template("main/index.html", context=context)
+
+
+@app.route("/animal/<int:animal_id>")
+def animal_card(animal_id):
+    animal = controller.get_animal()
+    context = {"animal": animal}
+    return render_template("main/animal.html", context=context)
+
+
+@app.route("/animal/<int:animal_id>/<procedure>")
+def procedure_list(animal_id, procedure):
+    procedures = controller.get_procedure_list()
+    context = {"procedures": procedures}
+    return render_template("main/procedure_list.html", context=context)
 
 
 @app.route("/animal")
-def animal():
-    return render_template("main/animal.html")
-
-
-@app.route("/animal/procedure_list")
-def procedure_list():
-    return render_template("main/procedure_list.html")
-
-
-@app.route("/add_animal")
 def add_animal():
     return render_template("main/add_animal.html")
 
